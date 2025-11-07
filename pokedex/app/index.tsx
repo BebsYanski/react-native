49b86b0ce40163207038c9f94a81db0f97806c4e
BebsYanski <bebongnchuy@gmail.com>
2025-11-07 12:00:06 +0100

    add pokemons

--- pokedex/app/index.tsx
+++ pokedex/app/index.tsx
@@ -1,15 +1,122 @@
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+
+interface Pokemon {
+  name: string;
+  image: string;
+  imageBack: string;
+  types: PokemonType[];
+}
+interface PokemonType {
+  type: {
+    name: string;
+    url: string;
+  };
+}
+const colorsByType = {
+  normal: "#A8A77A",
+  fire: "#EE8130",
+  water: "#6390F0",
+  electric: "#F7D02C",
+  grass: "#7AC74C",
+  ice: "#96D9D6",
+  fighting: "#C22E28",
+  poison: "#A33EA1",
+  ground: "#E2BF65",
+  flying: "#A98FF3",
+  psychic: "#F95587",
+  bug: "#A6B91A",
+  rock: "#B6A136",
+  ghost: "#735797",
+  dragon: "#6F35FC",
+  dark: "#705746",
+  steel: "#B7B7CE",
+  fairy: "#D685AD",
+};
 
 export default function Index() {
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+  console.log(JSON.stringify(pokemons[0], null, 2));
+
+  useEffect(() => {
+    /* fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+      .then((res) => res.json())
+      .then((data) => setPokemons(data.results)); */
+    fetchPokemons();
+  }, []);
+
+  async function fetchPokemons() {
+    try {
+      const response = await fetch(
+        "https://pokeapi.co/api/v2/pokemon?limit=151"
+      );
+      const data = await response.json();
+
+      // Fetch the details of each pokemon
+      const pokemonPromises = await Promise.all(
+        data.results.map(async (pokemon: any) => {
+          const res = await fetch(pokemon.url);
+          const resData = await res.json();
+          return {
+            name: pokemon.name,
+            image: resData.sprites.front_default,
+            imageBack: resData.sprites.back_default,
+            types: resData.types,
+          };
+        })
+      );
+
+      /*  const pokemonDetails = await Promise.all(pokemonPromises);
+      console.log(pokemonDetails); */
+
+      setPokemons(pokemonPromises);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <ScrollView contentContainerStyle={{ gap: 16, padding: 16 }}>
+      {pokemons.map((pokemon) => (
+        <View
+          key={pokemon.name}
+          style={{
+            // @ts-ignore
+            backgroundColor: colorsByType[pokemon.types[0].type.name] + 50,
+            padding: 20,
+            borderWidth: 0,
+            borderRadius: 20,
+          }}
+        >
+          <Text style={styles.name}>{pokemon.name}</Text>
+          <Text>{pokemon.types[0].type.name}</Text>
+
+          <View style={{ margin: 10, flexDirection: "row" }}>
+            <Image
+              source={{ uri: pokemon.image }}
+              style={{ width: 150, height: 150 }}
+            />
+            <Image
+              source={{ uri: pokemon.imageBack }}
+              style={{ width: 150, height: 150 }}
+            />
+          </View>
+        </View>
+      ))}
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  name: {
+    fontSize: 28,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  type: {
+    fontSize: 20,
+    color: "gray",
+    textAlign: "center",
+  },
+});
