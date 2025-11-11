227a645786e9ecdae90c43685303dff55cf45220
BebsYanski <bebongnchuy@gmail.com>
2025-11-11 11:51:11 +0100

    pokedex

--- pokedex/app/details.tsx
+++ pokedex/app/details.tsx
@@ -1,8 +1,30 @@
-import { ScrollView, StyleSheet } from "react-native";
+import { Stack, useLocalSearchParams } from "expo-router";
+import { useEffect, useState } from "react";
+import { ScrollView, StyleSheet, Text } from "react-native";
 
 export default function Details() {
+  const params = useLocalSearchParams();
+  const [pokemonDetails, setPokemonDetails] = useState({});
+
+  useEffect(() => {}, []);
+
+  async function fetchPokemon(name: string) {
+    try {
+      const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+      const data = await res.json();
+      setPokemonDetails(data);
+    } catch (e) {
+      console.log("Error fetching Pokemon");
+    }
+  }
+
   return (
-    <ScrollView contentContainerStyle={{ gap: 16, padding: 16 }}></ScrollView>
+    <>
+      <Stack.Screen options={{ title: params.name as string }} />
+      <ScrollView contentContainerStyle={{ gap: 16, padding: 16 }}>
+        <Text>{params.name}</Text>
+      </ScrollView>
+    </>
   );
 }
 
